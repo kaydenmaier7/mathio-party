68cf6ccd852a1cf323e6c7420ca0c01c9c53a17c
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-10-15 21:26:52 -0700

    Point requests to heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mathio-party_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Set default port
+  config.action_mailer.default_url_options = { host: 'mathio-party.herokuapp.com'}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
