4f6361349b560e691972b8f82d22c380f701fcd2
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-10-15 18:08:48 -0700

    Add game#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get 'games/index'
   get 'games/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'game#index'
 end
