c61966672c8ada527749bd711d9d1254756bfbc2
Louis Rowan <email": null>
2016-10-17 12:09:27 -0700

    adds functionality to save player 2 id for frogger ajax call

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,9 +7,10 @@ class ResultsController < ApplicationController
       player_1 = current_user.id
 
       player1result = Result.new(user_id: player_1, game_id: 1, correct: params[:player1correct], incorrect: params[:player1wrong])
-      # player2result = Result.new(user_id: player_2, game_id: 1, correct: params[:player2correct], incorrect: params[:player2wrong])
+      player2result = Result.new(user_id: params[:player2], game_id: 1, correct: params[:player2correct], incorrect: params[:player2wrong])
       puts "*"*100
       p player1result
+      p player2result
 
     end
 
