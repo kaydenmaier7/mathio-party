06cfcbceb66298ddd1569d9b18cf093737d1bbe5
Chris Johnson <johnsonc.88@gmail.com>
2016-10-18 10:42:52 -0700

    add image assets for invasion-game

--- public/javascripts/invasion.js
+++ public/javascripts/invasion.js
@@ -7,3 +7,11 @@ var loadGame = function(){
   $('#invasion').html('')
   game = new Phaser.Game(1000, 600, Phaser.AUTO, 'invasion');
 }
+
+function preload() {
+  game.load.image("background", "/assets/invasion/farm.png");
+  game.load.image('cow', '/assets/invasion/cow.png');
+  game.load.image('ufo1', '/assets/invasion/ufo1.png');
+  game.load.image('ufo2', '/assets/invasion/ufo2.png');
+  game.load.image('beam', '/assets/invasion/beam.png');
+}
