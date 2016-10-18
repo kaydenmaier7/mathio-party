7eede4f509caab5549e04dcdaae635777e96f308
Chris Johnson <johnsonc.88@gmail.com>
2016-10-17 22:03:22 -0700

    update percent method to find results by sub_skill

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
   has_many :results
   has_many :matches, through: :results
   has_many :games, through: :matches
-  has_many :sub_skills, through: :results 
+  has_many :sub_skills, through: :results
 
-  def percent(result, skill)
-    results = self.results.where(skill: skill)
+  def percent(result, skill, sub_skill)
+    # fetch the sub_skills with the skill: 'skill' and name: 'sub_skill'
+    results = self.sub_skills.where(skill_name: skill).where(name: sub_skill)
     correct = results.pluck(:correct).inject { |sum, n| sum + n }.to_f
     incorrect = results.pluck(:incorrect).inject { |sum, n| sum + n }.to_f
     total = correct + incorrect
