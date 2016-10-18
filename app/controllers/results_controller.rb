7d24c288dc83ff7a2325516f3fda72f1e9b0bfed
Louis Rowan <email": null>
2016-10-18 14:32:19 -0700

    adds logic to save skills and subskills for frogger

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,16 +1,11 @@
 class ResultsController < ApplicationController
   ActionController::Parameters.permit_all_parameters = true
+  include ResultsHelper
 
   def create
     if request.xhr?
 
-      player_1 = current_user.id
-
-      player1result = Result.new(user_id: player_1, game_id: 1, correct: params[:player1correct], incorrect: params[:player1wrong])
-      player2result = Result.new(user_id: current_player2.id, game_id: 1, correct: params[:player2correct], incorrect: params[:player2wrong])
-      puts "*"*100
-      p player1result
-      p player2result
+      create_match(params)
 
     end
 
