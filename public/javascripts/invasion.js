57f87fa81797d1d8f49ae69f1159a1af74edf3c0
Chris Johnson <johnsonc.88@gmail.com>
2016-10-18 11:19:13 -0700

    refactor preload for scalability and add comments

--- public/javascripts/invasion.js
+++ public/javascripts/invasion.js
@@ -2,22 +2,32 @@ $(document).ready(function(){
   loadGame();
 })
 
-
+// add game window to page
 var loadGame = function(){
   $('#invasion').html('')
   game = new Phaser.Game(1000, 600, Phaser.AUTO, 'invasion', {preload: preload, create: create});
 }
 
+// declare static assets
+var assets = [
+  ["background", "/assets/invasion/farm.png"],
+  ['cow', '/assets/invasion/cow.png'],
+  ['ufo1', '/assets/invasion/ufo1.png'],
+  ['ufo2', '/assets/invasion/ufo2.png'],
+  ['beam', '/assets/invasion/beam.png']
+]
+
+// load static assets
 function preload() {
-  game.load.image("background", "/assets/invasion/farm.png");
-  game.load.image('cow', '/assets/invasion/cow.png');
-  game.load.image('ufo1', '/assets/invasion/ufo1.png');
-  game.load.image('ufo2', '/assets/invasion/ufo2.png');
-  game.load.image('beam', '/assets/invasion/beam.png');
+  assets.forEach(function(asset){
+    var name = asset[0];
+    var location = asset[1];
+    game.load.image(name, location);
+  })
 }
 
+// add background to the game
 function create(){
     game.add.tileSprite(0, 0, 1000, 600, 'background');
-    game.physics.startSystem(Phaser.Physics.ARCADE);
 }
 
