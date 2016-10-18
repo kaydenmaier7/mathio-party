3db7eaadbb999f65d6b3be00823ed82a4d34cf44
Chris Johnson <johnsonc.88@gmail.com>
2016-10-17 17:05:12 -0700

    add results and game2 to the seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,20 @@ game1 = {
 	directions: 'You do the thing and stuff happens!'
 }
 
+game2 = {
+  name: 'Other Game',
+  src: '/javascripts/game2',
+  description: 'A second game!',
+  directions: 'Like the other game but completely different'
+}
+
 
 User.create(user1)
 Game.create(game1)
+Game.create(game2)
 
 10.times do |num|
-  30.times do
+  10.times do
     Result.create({skill: "#{num}times_table",
         correct: rand(2..30),
         incorrect: rand(2..30),
@@ -25,4 +33,16 @@ Game.create(game1)
      })
   end
 end
+
+10.times do |num|
+  10.times do
+    Result.create({skill: "#{num}times_table",
+        correct: rand(2..30),
+        incorrect: rand(2..30),
+        user_id: 2,
+        game_id: 1
+     })
+  end
+end
+
 
