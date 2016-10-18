fc3184e2b710f1353d2a8f2bb542da18bc4c0a08
Chris Johnson <johnsonc.88@gmail.com>
2016-10-18 12:21:16 -0700

    add player ships to the game

--- public/javascripts/invasion.js
+++ public/javascripts/invasion.js
@@ -19,7 +19,7 @@ var assets = [
 
 // make an array to track current cows
 var herd = [];
-var cow;
+var cow, player1, player2;
 
 // load static assets
 function preload() {
@@ -37,6 +37,14 @@ function create(){
   // create cow group
   cow = game.add.group();
   spawnCow(Math.random()*(game.width - 100) , Math.random()*(game.height/2) + game.height* 0.3);
+
+  // Player 1
+  player1 = game.add.group();
+  createPlayer1(400, 10, 'ufo1');
+
+  // Player 2
+  player2 = game.add.group();
+  createPlayer2(200, 10, 'ufo2');
 };
 
 // create a cow
@@ -44,3 +52,13 @@ function spawnCow(x, y){
   var value = Math.floor( Math.random() * 11 )
   cow.create(x, y, 'cow', value)
 };
+
+function createPlayer1(x, y, id){
+  var player = player1.create(x, y, id);
+  player.player_id = id;
+}
+
+function createPlayer2(x, y, id){
+  var player = player2.create(x, y, id);
+  player.player_id = id;
+}
