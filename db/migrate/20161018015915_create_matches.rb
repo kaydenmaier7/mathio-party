a161430f3d22e9f3bfcf10b29c4a44f01d4ab772
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-10-17 20:30:03 -0700

    Update mathches migration

--- db/migrate/20161018015915_create_matches.rb
+++ db/migrate/20161018015915_create_matches.rb
@@ -2,7 +2,6 @@ class CreateMatches < ActiveRecord::Migration[5.0]
   def change
     create_table :matches do |t|
       t.references :game, foreign_key: true
-      t.references :skill, foreign_key: true
 
       t.timestamps
     end
