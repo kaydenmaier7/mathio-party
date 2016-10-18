a3524757d3ac5107e966fccb6b934df10feb9723
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-10-17 17:11:51 -0700

    Add functionality to create route.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,8 +3,21 @@ class PlayersController < ApplicationController
   end
 
   def create
+  	@current_player2 = User.find_by(email: player_params[:email])
+  	if @current_player2 && @current_player2.valid_password?(player_params[:password])
+  		session[:player2] = @current_player2.id
+  		redirect_to root_path
+  	else
+  		@errors = ['Invalid login']
+  		render 'new'
+  	end
   end
 
   def destroy
+  end
+
+  private
+  def player_params
+  	params.require(:player).permit(:email, :password)
   end
 end
