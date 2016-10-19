456cd14ffc838a41c128bdb91d4106f9dcccb6a1
K. Mannah Kallon <kenikall@gmail.com>
2016-10-18 18:36:01 -0700

    Adds ammo mechanic

--- public/javascripts/duck_hunt.js
+++ public/javascripts/duck_hunt.js
@@ -8,21 +8,24 @@ var mainState= {
     game.load.image('p2', '/images/duck_hunt/p2crosshairs.png');
     game.load.image('shot', '/images/duck_hunt/shotCrosshairs.png');
     //numbers
-    game.load.image('one', '/images/duck_hunt/1.png')
-    game.load.image('two', '/images/duck_hunt/2.png')
-    game.load.image('three', '/images/duck_hunt/3.png')
-    game.load.image('four', '/images/duck_hunt/4.png')
-    game.load.image('five', '/images/duck_hunt/5.png')
-    game.load.image('six', '/images/duck_hunt/6.png')
-    game.load.image('seven', '/images/duck_hunt/7.png')
-    game.load.image('eight', '/images/duck_hunt/8.png')
-    game.load.image('nine', '/images/duck_hunt/9.png')
-    game.load.image('ten', '/images/duck_hunt/10.png')
+    game.load.image('one', '/images/duck_hunt/1.png');
+    game.load.image('two', '/images/duck_hunt/2.png');
+    game.load.image('three', '/images/duck_hunt/3.png');
+    game.load.image('four', '/images/duck_hunt/4.png');
+    game.load.image('five', '/images/duck_hunt/5.png');
+    game.load.image('six', '/images/duck_hunt/6.png');
+    game.load.image('seven', '/images/duck_hunt/7.png');
+    game.load.image('eight', '/images/duck_hunt/8.png');
+    game.load.image('nine', '/images/duck_hunt/9.png');
+    game.load.image('ten', '/images/duck_hunt/10.png');
+    //bullets
+    game.load.image('bullet', '/images/duck_hunt/bullet.png');
     //sounds
     game.load.audio('shotSound', '/sounds/duck_hunt/shot.wav')
     game.load.audio('quacks', '/sounds/duck_hunt/quacks.wav')
     game.load.audio('hit', '/sounds/duck_hunt/hit.wav')
     game.load.audio('fall', '/sounds/duck_hunt/fall.wav')
+    game.load.audio('click', '/sounds/duck_hunt/click.wav')
   },
   create: function(){
     //set stage
@@ -47,18 +50,25 @@ var mainState= {
     this.inner2.anchor.setTo( 0.5, 0.5 );
     game.physics.arcade.enable(this.p2);
 
+    this.p1.canShoot = true;
+    this.p2.canShoot = true;
+
     //move input keys
     this.p1up    = game.input.keyboard.addKey(Phaser.Keyboard.W);
     this.p1down  = game.input.keyboard.addKey(Phaser.Keyboard.S);
     this.p1right = game.input.keyboard.addKey(Phaser.Keyboard.D);
     this.p1left  = game.input.keyboard.addKey(Phaser.Keyboard.A);
-    this.p1shoot = game.input.keyboard.addKey(Phaser.Keyboard.SPACE);
+    this.p1shoot = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     this.p2up    = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     this.p2down  = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
     this.p2right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     this.p2left  = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     this.p2shoot = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+
+    //initialize bullets
+    this.reload();
+    this.bullets();
   },
   update: function(){
     // keeps target center
@@ -68,13 +78,34 @@ var mainState= {
   },
 
   reload: function(){
-    this.p1bullets =[1,1,1];
-    this.p2bullets =[1,1,1];
-  }
+    this.p1bullets = [1,1,1];
+    this.p2bullets = [1,1,1];
+
+    this.p1b1 = game.add.sprite( 100, 850, 'bullet');
+    this.p1b2 = game.add.sprite( 150, 850, 'bullet');
+    this.p1b3 = game.add.sprite( 200, 850, 'bullet');
+
+    this.p2b1 = game.add.sprite( 775, 850, 'bullet');
+    this.p2b2 = game.add.sprite( 825, 850, 'bullet');
+    this.p2b3 = game.add.sprite( 875, 850, 'bullet');
+  },
+
+  fireBullets: function(player){
+    if ( player === 1 ){
+      if(this.p2b3){
+        this.p2b3.destroy();
+      }else if(this.p2b2){
+        this.p2b2.destroy();
+      }else{
+        this.p2b1.destroy();
+      }
+    }
+
+  },
 
   // player 1 movement
   move: function(){
-     if (this.p1up.isDown) { this.p1.y -= 3; }
+    if (this.p1up.isDown) { this.p1.y -= 3; }
     else if (this.p1down.isDown) { this.p1.y += 3; }
     if (this.p1right.isDown) { this.p1.x += 3; }
     else if (this.p1left.isDown) { this.p1.x -= 3; }
@@ -85,10 +116,25 @@ var mainState= {
     else if (this.p2left.isDown) { this.p2.x -= 3; }
   },
 
-  shoot:function(){
-    if (p1shoot){
-      shot = game.add.sprite(this.p1.x, this.p1.y, 'shot')
-      setTimeout(function(){shot.kill},100);
+  shoot: function(){
+    if (this.p1shoot.isDown && this.p1.canShoot && this.p1bullets.length > 0){
+      this.p1bullets.pop();
+      this.p1.canShoot = false;
+      that = this;
+      setTimeout(function(){that.p1.canShoot = true}, 1000)
+      this.shot1 = game.add.sprite(this.p1.x, this.p1.y, 'shot');
+      this.shot1.anchor.setTo( 0.5, 0.5);
+      setTimeout(function(){that.shot1.kill()},100);
+      this.shotSound.play();
+    }
+    if (this.p2shoot.isDown && this.p2.canShoot && this.p2bullets.length > 0){
+      this.p2bullets.pop();
+      this.p2.canShoot = false;
+      that = this;
+      setTimeout(function(){that.p2.canShoot = true}, 1000)
+      this.shot2 = game.add.sprite(this.p2.x, this.p2.y, 'shot');
+      this.shot2.anchor.setTo( 0.5, 0.5);
+      setTimeout(function(){that.shot2.kill()},100);
       this.shotSound.play();
     }
   },
@@ -98,6 +144,10 @@ var mainState= {
     this.inner1.y = this.p1.y;
     this.inner2.x = this.p2.x;
     this.inner2.y = this.p2.y;
+  },
+
+  spawnDuck: function(){
+
   }
 };
 
