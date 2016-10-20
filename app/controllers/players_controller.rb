bf026ebc277ad15851ce0d4dc07522153eed6ca6
Chris Johnson <johnsonc.88@gmail.com>
2016-10-19 18:58:13 -0700

    style player2 login and flash-messages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,15 +5,15 @@ class PlayersController < ApplicationController
   def create
   	@current_player2 = User.find_by(email: player_params[:email])
     if @current_player2 == current_user
-      @errors = ['You are already signed in!']
+      flash[:errors] = ['You are already signed in!']
       return render 'new'
     end
-    
+
   	if @current_player2 && @current_player2.valid_password?(player_params[:password])
   		session[:player2] = @current_player2.id
   		redirect_to root_path
   	else
-  		@errors = ['Invalid login']
+  		flash[:errors] = ['Invalid login']
   		render 'new'
   	end
   end
